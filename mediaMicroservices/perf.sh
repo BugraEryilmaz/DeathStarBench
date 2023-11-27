a43b0ea18535ff0d63dfd32d60ee7a70843ad4a4
Bugra Eryilmaz <bugra.eryilmaz@epfl.ch>
2023-11-27 22:23:48 +0100

    perf script

---
+++ mediaMicroservices/perf.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# get name of the experiment from command line argument
+# have default value of "default" if no argument is passed
+if [ -z "$1" ]
+then
+    echo "No argument supplied"
+    number=$(ls -l results/ | wc -l)
+    name="default-$number"
+else
+    name=$1
+fi
+
+sudo ./remote-perf-https-multicore-x86.sh 1 24
+
+# move results to results folder
+mkdir -p results
+mv collection.csv results/collection-$name.csv
